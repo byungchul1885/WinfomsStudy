5ba5dcb2a1940ec49165adad7840fdbaadfa908e
bccho <byungchul1885@gmail.com>
2020-02-05 20:17:46 +0900

    tutorial one - final

--- HelloWorld/HelloWorld/Form1.Designer.cs
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -29,32 +29,46 @@
         private void InitializeComponent()
         {
             this.btnClickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnClickThis
             // 
-            this.btnClickThis.Location = new System.Drawing.Point(70, 71);
+            this.btnClickThis.Location = new System.Drawing.Point(86, 77);
             this.btnClickThis.Name = "btnClickThis";
             this.btnClickThis.Size = new System.Drawing.Size(110, 54);
             this.btnClickThis.TabIndex = 0;
             this.btnClickThis.Text = "Click this";
             this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(121, 153);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(38, 12);
+            this.lblHelloWorld.TabIndex = 1;
+            this.lblHelloWorld.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 337);
+            this.ClientSize = new System.Drawing.Size(509, 403);
+            this.Controls.Add(this.lblHelloWorld);
             this.Controls.Add(this.btnClickThis);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
     }
 }
 
